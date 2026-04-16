438f7735abc14466a7e8a10302e2f2c85aaf51df
Saidas2020 <morvekarsai@gmail.com>
2026-04-16 11:29:43 +0530

    created dtos

---
+++ TaskManagementBoard.api/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementBoard.api.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(300)]
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<TaskItem>? Tasks { get; set; }
+    }
+}
---
+++ TaskManagementBoard.api/Models/TaskItem.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementBoard.api.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+        public Project? Project { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        [Required]
+        public string Priority { get; set; } // keep string for speed
+
+        [Required]
+        public string Status { get; set; }
+
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public List<Comment>? Comments { get; set; }
+    }
+}

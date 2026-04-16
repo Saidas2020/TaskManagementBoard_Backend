2ffffbcd0a7693da91243b7ca35d013f7596e959
Saidas2020 <morvekarsai@gmail.com>
2026-04-16 11:38:07 +0530

    done

---
+++ TaskManagementBoard.api/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagementBoard.api.Data;
+using TaskManagementBoard.api.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=taskboard.db"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await SeedData.Initialize(db);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.MapControllers();
+
+app.Run();
